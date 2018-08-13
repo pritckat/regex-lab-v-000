9dfc2c4e8d2e47ff739a2cc808e7b4d9ce3890dd
Katrina Pritchard <pritckat@gmail.com>
2018-08-13 20:50:29 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
 def starts_with_a_vowel?(word)
-  word.scan(/^[aeiouAEIOU]+[a-zA-Z]*/)
+  if word.scan(/^[aeiouAEIOU]+[a-zA-Z]*/)
+    return true
+  else
+    return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
